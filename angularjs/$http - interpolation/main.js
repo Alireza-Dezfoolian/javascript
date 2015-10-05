d2a3cc1a065712c7b5786a9ce3da7e3999b83b6c
alireza <dezfoolian@gmail.com>
2015-10-05 23:17:37 +1100

    add   string

--- angularjs/$http - interpolation/main.js
+++ angularjs/$http - interpolation/main.js
@@ -1,9 +1,9 @@
 var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+app.controller('customersCtrl', ["$scope", "$http", function($scope, $http) {
     $http.get("customers.json").success(function(response) {
         for (obj in response.records) {
             //console.log(obj, response.records[obj]["City"]);
         }
         $scope.names = response.records;
     });
-});
+}]);
