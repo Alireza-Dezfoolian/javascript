096570e866c3b518f9b9683fb0b2005c3fe7c928
alireza <dezfoolian@gmail.com>
2015-11-29 11:41:56 +1100

    start the plugin
    lib

---
+++ ownlibrary/library.js
@@ -0,0 +1,22 @@
+function $(selector){
+ var self = {};
+ self.selector = selector;
+ self.element = document.querySelector(self.selector);
+
+ self.html = function(){
+ 	return self.element;
+ }
+
+self.attr = function(name, value){
+	if (!value) return self.element.getAttribute(name);
+	self.element.setAttribute(name, value);
+	return self;
+
+}
+
+self.on = function(type, callback){
+  self.element['on' + type] = callback;
+  return self;
+}
+ return self;
+}
